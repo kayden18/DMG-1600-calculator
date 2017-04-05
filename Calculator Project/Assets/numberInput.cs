1e9a02ac1056c2a8b935ae19f5684bab82c28bab
Unknown <kayden18@ymail.com>
2017-04-05 10:53:13 -0600

    setup
    setup buttons and input fields and started input field script

---
+++ Calculator Project/Assets/numberInput.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class numberInput : MonoBehaviour {
+
+    public InputField firstnumber;
+    public InputField secondnumber;
+    
+}
