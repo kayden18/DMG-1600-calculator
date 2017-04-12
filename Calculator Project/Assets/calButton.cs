8ea8c58abb37fed239449db7c0c42a5b8a8998c2
Unknown <kayden18@ymail.com>
2017-04-11 19:10:08 -0600

    add number buttons and script to disable buttons

--- Calculator Project/Assets/calButton.cs
+++ Calculator Project/Assets/calButton.cs
@@ -1,22 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class calButton : MonoBehaviour {
 
-    public numberInput leftNum;
-    public numberInput rightNum;
+    public InputField input;
+    public InputField input2;
+    public Text Answer;
+    public Text buttonNum;
 
+    public void button5Click()
+    {
+        
+    }
     public void buttonClick() {
-        print(leftNum.numberOnetoFloat() + rightNum.numberOnetoFloat());
-           }
-    public void button2Click(){
-        print(leftNum.numberOnetoFloat() - rightNum.numberOnetoFloat());
+        int numberOutPut;
+        numberOutPut = int.Parse(input.text) + int.Parse(input2.text);
+        Answer.text = numberOutPut.ToString();
+        gameObject.GetComponent<Button>().interactable = false;
     }
-    public void button3click(){
-        print(leftNum.numberOnetoFloat() * rightNum.numberOnetoFloat());
+    public void button2Click() {
+        int numberOutPut;
+        numberOutPut = int.Parse(input.text) - int.Parse(input2.text);
+        Answer.text = numberOutPut.ToString();
+        gameObject.GetComponent<Button>().interactable = false;
     }
-    public void button4click(){
-        print(leftNum.numberOnetoFloat() / rightNum.numberOnetoFloat());
+    public void button3click() {
+        int numberOutPut;
+        numberOutPut = int.Parse(input.text) * int.Parse(input2.text);
+        Answer.text = numberOutPut.ToString();
+        gameObject.GetComponent<Button>().interactable = false;
     }
+    public void button4click() {
+        int numberOutPut;
+        numberOutPut = int.Parse(input.text) / int.Parse(input2.text);
+        Answer.text = numberOutPut.ToString();
+        gameObject.GetComponent<Button>().interactable = false;
+    }
+ 
 }
