1153ab1aae61f47584d0e81e7eb03fb6e37f03d2
Unknown <kayden18@ymail.com>
2017-04-29 18:03:54 -0600

    fixed for each loop

--- Calculator Project/Assets/calButton.cs
+++ Calculator Project/Assets/calButton.cs
@@ -2,12 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class calButton : MonoBehaviour {
 
     public InputField input;
     public InputField input2;
     public Text Answer;
-   
+    public List<Text> randNum;
+
+    static private bool chooseInputField = true;
+    public int numsToInput;
+
+    public void boolClick(){
+        chooseInputField = true;
+    }
+    public void bool2Click(){
+        chooseInputField = false;
+    }
+
+    public void SteveHatesYou()
+    {
+        foreach (Text randInt in randNum)
+        {
+              if (Answer.text == randInt.text)
+            {
+               print("answers match");
+               randInt.enabled = false;
+            }
+        }
+    }
+
     public void buttonClick() {
         int numberOutPut;
         numberOutPut = int.Parse(input.text) + int.Parse(input2.text);
@@ -31,5 +55,16 @@ public class calButton : MonoBehaviour {
         numberOutPut = int.Parse(input.text) / int.Parse(input2.text);
         Answer.text = numberOutPut.ToString();
         gameObject.GetComponent<Button>().interactable = false;
+    }
+    public void button5Click()
+    {
+        if (chooseInputField)
+        {
+            input.text += numsToInput.ToString();
+        }
+        else
+        {
+            input2.text += numsToInput.ToString();
+        }
     }
 }
---
+++ Calculator Project/Assets/randomNum.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class randomNum : MonoBehaviour
+{
+    
+    private int randomNumGenerator;
+    public Text randInt;
+
+    public void Start()
+    {
+        randomNumGenerator = Random.Range(1, 10);
+        randInt.text = randomNumGenerator.ToString();
+    }
+}
